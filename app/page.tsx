4ccd0bad05d39b25f71487e19c56a98e2eb1638e
Mayur Pal <thissidemayur@gmail.com>
2025-08-01 00:37:17 +0530

    feat(footer): implement footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./componets/Navbar";
 import Hero from "./componets/Hero";
+import Footer from "./componets/Footer";
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
       <Navbar />
       <Hero />
+      <Footer />
     </main>
   );
 }
