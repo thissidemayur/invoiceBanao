26894659cee1cde2861706e882bff2b7b285acd5
Mayur Pal <thissidemayur@gmail.com>
2025-07-31 23:32:28 +0530

    refactor: remove non use declearation

--- app/componets/SubmitButton.tsx
+++ app/componets/SubmitButton.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import React from "react";
 import { useFormStatus } from "react-dom";
-useFormStatus;
 
 interface iAppProps {
   text: string;
